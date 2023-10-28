d316dfe8505c3ebecfc77327ac4d4e6fe27d5c54
Seo sangmin <139242512+smpringles24@users.noreply.github.com>
2023-10-29 03:52:31 +0900

    :sparkles: Change screen configuration to match added backend data & dateformat change (#6)
    .

--- lib/data/notice_board_model.dart
+++ lib/data/notice_board_model.dart
@@ -1,16 +1,26 @@
 class NoticeBoardModel {
   int? id;
   String? title;
+  String? writer;
+  int? view;
   String? contents;
   String? createdAt;
   String? updatedAt;
 
   NoticeBoardModel(
-      {this.id, this.title, this.contents, this.createdAt, this.updatedAt});
+      {this.id,
+      this.title,
+      this.writer,
+      this.view,
+      this.contents,
+      this.createdAt,
+      this.updatedAt});
 
   NoticeBoardModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     title = json['title'];
+    writer = json['writer'];
+    view = json['view'];
     contents = json['contents'];
     createdAt = json['createdAt'];
     updatedAt = json['updatedAt'];
@@ -20,6 +30,8 @@ class NoticeBoardModel {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
     data['title'] = title;
+    data['writer'] = writer;
+    data['view'] = view;
     data['contents'] = contents;
     data['createdAt'] = createdAt;
     data['updatedAt'] = updatedAt;
